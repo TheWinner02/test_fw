8e39666f22d74527f24d440b6e60ef8f50478f46
TheWinner02 <124472504+TheWinner02@users.noreply.github.com>
2025-04-11 15:54:06 +0000

    fix code

---
+++ script/system/fix.sh
@@ -0,0 +1,6 @@
+# Copia Fix file nella rom
+
+cp -r -v fix/OneUI/UI6.1/system/'Boot Fix'/system/system/* a53/ROM/system/system
+cp -r -v fix/OneUI/UI6.1/system/'Camera Fix'/system/system/* a53/ROM/system/system
+cp -r -v fix/OneUI/UI6.1/system/'Hotspot Fix'/system/system/* a53/ROM/system/system
+cp -r -v fix/OneUI/UI6.1/system/'OCR Fix'/system/system/* a53/ROM/system/system
