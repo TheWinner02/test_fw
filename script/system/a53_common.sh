0838fdcdb589538b83a0a61d0d2c2c17ebd281a1
TheWinner02 <124472504+TheWinner02@users.noreply.github.com>
2025-04-22 16:22:19 +0000

    improve

--- script/system/a53_common.sh
+++ script/system/a53_common.sh
@@ -1,4 +1,5 @@
 # Copia common file nella rom
 
 cp -r -v common/system/* a53/ROM/system/system
+cp -r -v common/vendor/exy7904/* a53/ROM/vendor
 cp -r -v a53/common/system/* a53/ROM/system/system
--- script/system/s23fe_common.sh
+++ script/system/s23fe_common.sh
@@ -1,4 +1,6 @@
 # Copia common file nella rom
 
 cp -r -v common/system/* s23fe/ROM/system/system
+cp -r -v common/vendor/exy7904/* s23fe/ROM/vendor
+cp -r -v common/vendor/exy7884/* s23fe/ROM/device/exy7884/vendor
 cp -r -v s23fe/common/system/* s23fe/ROM/system/system
